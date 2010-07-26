809afbf6616153f44613c71157ae5f824ce42e6e
Adrian Cuadros <adrian@innku.com>
2010-07-26 17:32:13 -0500

    Changes the rb files to the gems directory

--- lib/rateable_engine.rb
+++ lib/rateable_engine.rb
@@ -1,8 +1,8 @@
 module RateableEngine
   
   if defined?(Rails) && Rails::VERSION::MAJOR == 3
-    require 'rateable/acts_as_rateable'
-    require 'rateable/engine'
+    require 'rateable_engine/acts_as_rateable'
+    require 'rateable_engine/engine'
   end
   
 end
