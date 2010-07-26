8233d04d64d6f7dcf983a19db7f28cb249e8aaf1
Adrian Cuadros <adrian@innku.com>
2010-07-26 18:08:43 -0500

    Takes the models into the namespaced plugin

--- app/models/rating.rb
+++
@@ -1,7 +0,0 @@
-module RateableEngine
-  class Rating < ActiveRecord::Base
-    belongs_to :rate
-    belongs_to :rateable, :polymorphic => true
-    validates_uniqueness_of :user_id, :scope => [:rateable_id, :rateable_type]
-  end
-end
